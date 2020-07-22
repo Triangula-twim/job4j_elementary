83618b0d621adae4e42d9a5202cf551690bd980c
FrolovYO <yuriy_frolov_2019@mail.ru>
2020-07-22 12:56:51 +0300

    4.2. Тернарное сравнение [#331198]

---
+++ src/ru/job4j/condition/MaxTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MaxTest {
+    @Test
+    public void whenMax1To2Then2() {
+        int result = Max.max(1, 2);
+        assertThat(result, is(2));
+    }
+
+    @Test
+    public void whenMax3To2Then3() {
+        int result = Max.max(3, 2);
+        assertThat(result, is(3));
+    }
+
+    @Test
+    public void whenMax2To6Then6() {
+        int result = Max.max(2, 6);
+        assertThat(result, is(6));
+    }
+
+    @Test
+    public void whenMax4To4Then4() {
+        int result = Max.max(4, 4);
+        assertThat(result, is(4));
+    }
+}

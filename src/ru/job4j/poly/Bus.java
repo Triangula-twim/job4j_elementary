5fcbdec5e40dc14cea36d09374ec2b423f2c2321
master <yuriy_frolov_2019@mail.ru>
2020-07-30 21:02:30 +0300

    3. Интерфейс Input [#331341]

---
+++ src/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void go() {
+
+    }
+
+    @Override
+    public void passenger(int countPassenger) {
+
+    }
+
+    @Override
+    public double fill(double countFuel) {
+        return 0;
+    }
+}

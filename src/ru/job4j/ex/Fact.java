4d16a723ce57aeaee526c1f0cc9c18071656f1d7
master <yuriy_frolov_2019@mail.ru>
2020-07-31 21:34:40 +0300

    0.3. Тестирование исключений с junit [#331360]

--- src/ru/job4j/ex/Fact.java
+++ src/ru/job4j/ex/Fact.java
@@ -1,17 +1,13 @@
 package ru.job4j.ex;
 
 public class Fact {
-    public static void main(String[] args) {
-
-    }
-
     public int calc(int n) {
         if (n < 0) {
-            throw new IllegalArgumentException("n must be more then zero.");
+            throw new IllegalArgumentException("N could not be less then 0");
         }
         int rsl = 1;
-        for (int index = 1; index <= n; index++) {
-            rsl *= index;
+        for (int index = 1; index != n; index++) {
+            rsl += index;
         }
         return rsl;
     }
